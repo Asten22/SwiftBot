615ce240d28cd66be623a0e8c14ed1a723c36b7a
Asten22 <2558938@brunel.ac.uk>
2026-03-19 21:38:53 +0000

    Update LogFileManager.java

--- LogFileManager.java
+++ LogFileManager.java
@@ -4,23 +4,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+// This class writes the valid hexadecimal values to a text file at the end.
+ 
+
 public class LogFileManager {
 
     public static void writeSortedHexLog(List<String> values) {
+
+        // This will Copy the values so the original list is not changed
         List<String> sortedValues = new ArrayList<>(values);
 
+        // This will Sort using the actual hexadecimal value, not alphabetical order
         sortedValues.sort((a, b) ->
                 Integer.compare(HexConverter.hexToDecimal(a), HexConverter.hexToDecimal(b)));
 
         File file = new File("task9_hex_log.txt");
 
         try (FileWriter writer = new FileWriter(file)) {
+
             for (String value : sortedValues) {
                 writer.write(value);
                 writer.write(System.lineSeparator());
             }
 
             System.out.println("Log file saved to: " + file.getAbsolutePath());
+
         } catch (IOException e) {
             System.out.println("Error writing log file: " + e.getMessage());
         }
